b8a317dc6efa64abdcfb1d5b59f1a59f48745a11
Arjunsinghsikarwar <140879142+Arjunsinghsikarwar@users.noreply.github.com>
2026-04-06 13:34:43 +0530

    Fix Issue In UserDetailInfo

--- src/main/java/com/example/FinanceDataProcessing/FinanceProject/Service/UserDetail/UserDetailInfo.java
+++ src/main/java/com/example/FinanceDataProcessing/FinanceProject/Service/UserDetail/UserDetailInfo.java
@@ -19,7 +19,7 @@ public class UserDetailInfo implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()));
     }
 
     @Override
